3cfef0c1ead3cd5651302187cb4a9f7d647b37aa
Elvis-Munene <gitau.elvis@student.moringaschool.com>
2022-08-20 00:55:42 +0300

    commit tree b67978c9f78d6e58cbe485a2d5b88d0cfcd47d76

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,16 @@
 
+class Book
+    attr_accessor :title, :author, :page_count, :genre
+    def initialize(title)
+        @title = title
+    end 
 
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
+
+b1 = Book.new("Sommouwer joe")
+b1.author = "Sommouwer"
+b1.page_count = 1
+b1.genre = "hasj"
